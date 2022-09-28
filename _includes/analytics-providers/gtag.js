f783fb60257ce606e9010e2cb18c94ed959a91f7
Connor Redpath <39319546+credpath@users.noreply.github.com>
2022-09-28 11:53:15 -0400

    Create gtag.js

---
+++ _includes/analytics-providers/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QN5D0J2686"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QN5D0J2686');
+</script>
